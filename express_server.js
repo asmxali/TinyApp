c3fbdd61e3a379ffb02b2b98b2046adbc58d2b4e
Amino Asmaa Ali <amino.asmaa.ali@gmail.com>
2020-02-17 22:57:15 +0000

    Filling Out the urls_index.ejs Template

--- express_server.js
+++ express_server.js
@@ -1,28 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+app.set("view engine", "ejs");
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
