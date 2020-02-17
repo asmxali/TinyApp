aea7d8d4347359700c380255ec744511b2466ad1
Amino Asmaa Ali <amino.asmaa.ali@gmail.com>
2020-02-17 20:58:24 +0000

    add a path to the server using express

---
+++ express_server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
