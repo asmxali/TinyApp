57bc2313f04d04ee7b4730840e9c62208a8ce4fa
Amino Asmaa Ali <amino.asmaa.ali@gmail.com>
2020-02-17 22:49:49 +0000

    removed errors

--- express_server.js
+++ express_server.js
@@ -15,6 +15,15 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
