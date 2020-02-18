a00b7cec5f0a4d3011e99f3ed2375ff6c6878061
Amino Asmaa Ali <amino.asmaa.ali@gmail.com>
2020-02-18 17:45:44 +0000

    adding nodemon and feature to delete urls from database

--- express_server.js
+++ express_server.js
@@ -13,7 +13,13 @@ const urlDatabase = {
 };
 
 function generateRandomString() {
-  randomstring.generate(6);
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+   
+  for (var i = 0; i < 6; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+   
+  return text;
 };
 
 app.get("/urls", (req, res) => {
@@ -31,9 +37,26 @@ app.get("/urls/:shortURL", (req, res) => {
   console.log(templateVars);
   res.render("urls_show", templateVars);
 });
+
+app.get("/u/:shortURL", (req, res) => {
+  longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let newShortLink = generateRandomString();
+  urlDatabase[newShortLink] = req.body.asma;
+  console.log(urlDatabase);
+  res.redirect(`/urls/${newShortLink}`);
+
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  console.log(urlDatabase);
+  res.redirect(`/urls`);
+
 });
 
 app.listen(PORT, () => {
