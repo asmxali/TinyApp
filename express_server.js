d383ae0af28360a2c16e9cfc816bf134166a47ef
Amino Asmaa Ali <amino.asmaa.ali@gmail.com>
2020-02-18 00:50:56 +0000

    creating function to generate random string of length 6

--- express_server.js
+++ express_server.js
@@ -1,22 +1,39 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const randomstring = require("randomstring");
+
+app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.set("view engine", "ejs");
+function generateRandomString() {
+  randomstring.generate(6);
+};
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   console.log(templateVars);
   res.render("urls_show", templateVars);
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
